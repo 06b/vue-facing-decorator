4978f36e4a596ead51219b6641fa65fd1f7822bd
Adrian D. Alvarez <adrian@adriandalvarez.com>
2025-03-13 11:31:46 -0400

    Debug uncaught typeerror

--- src/utils.ts
+++ src/utils.ts
@@ -102,15 +102,16 @@ export function toComponentReverse(obj: any) {
 
 export function getSuperSlot(obj: any) {
     let curr = Object.getPrototypeOf(obj)
-
+    // Uncaught TypeError: Cannot read properties of null (reading 'constructor')
+    if (curr !== null) {
         while (curr.constructor !== Base) {
             const slot = getSlot(curr)
             if (slot) {
                 return slot
             }
             curr = Object.getPrototypeOf(curr)
-        
         }
+    }
     return null
 }
 
